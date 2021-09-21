6d67988de6115dcb20b4666f88e957e689c5b61f
firanger <jvtjuan15@gmail.com>
2021-09-20 23:49:56 -0500

    ejer2

--- taller_3.py
+++ taller_3.py
@@ -39,3 +39,53 @@ print("el numero de autos rosa que entraron es: ", rosa)
 print("el numero de autos roja que entraron es: ", roja)
 print("el numero de autos verde que entraron es: ", verde)
 print("el numero de autos azul que entraron es: ", azul)"""
+
+"""2.  Un Zoólogo pretende determinar el porcentaje de animales que hay en las 
+siguiente categorias de edades: 0 a 1 año, de mas de 1 año y menos de 3 y 
+de  3  o  mas  años.  El  zoológico  todavía  no  está  seguro  del  animal  que  va 
+estudiar. Si se decide por elefantes solo tomará una muestra de 20 de ellos; 
+si se decide por jirafas, tomara 15 de muestras y si son chompancés tomará 
+40. """
+"""
+animal=input("ingrese el nombre del animal para las muestras\nelefante\njirafa\nchompancés\n ")
+ceroauno=0
+masdeunañoymenosdetres=0
+tresomasaños=0
+if (animal=="elefante"):
+    for i in range(20):
+        e=random.uniform(0,4)
+        if (e>=0 and e<=1):
+            ceroauno+=1
+        if (e>1 and e<3):
+            masdeunañoymenosdetres+=1
+        if (e>=3):
+            tresomasaños+=1
+    print("el porcentaje de especimenes de edades entre 0 y 1 años es del ", (ceroauno/20)*100,"%")
+    print("el porcentaje de especimenes de edades entre mayor a 1 y menor que 3 años es del ", (masdeunañoymenosdetres/20)*100, "%")
+    print("el porcentaje de especimenes de edades entre 3 y mas años es del ", (tresomasaños/20)*100, "%")
+
+if (animal=="jirafa"):
+    for i in range(20):
+        e=random.uniform(0,4)
+        if (e>=0 and e<=1):
+            ceroauno+=1
+        if (e>1 and e<3):
+            masdeunañoymenosdetres+=1
+        if (e>=3):
+            tresomasaños+=1
+    print("el porcentaje de especimenes de edades entre 0 y 1 años es del ", (ceroauno/15)*100,"%")
+    print("el porcentaje de especimenes de edades entre mayor a 1 y menor que 3 años es del ", (masdeunañoymenosdetres/15)*100, "%")
+    print("el porcentaje de especimenes de edades entre 3 y mas años es del ", (tresomasaños/15)*100, "%")
+if (animal=="chompancés"):
+    for i in range(20):
+        e=random.uniform(0,4)
+        if (e>=0 and e<=1):
+            ceroauno+=1
+        if (e>1 and e<3):
+            masdeunañoymenosdetres+=1
+        if (e>=3):
+            tresomasaños+=1
+    print("el porcentaje de especimenes de edades entre 0 y 1 años es del ", (ceroauno/40)*100,"%")
+    print("el porcentaje de especimenes de edades entre mayor a 1 y menor que 3 años es del ", (masdeunañoymenosdetres/40)*100, "%")
+    print("el porcentaje de especimenes de edades entre 3 y mas años es del ", (tresomasaños/40)*100, "%")  
+"""
